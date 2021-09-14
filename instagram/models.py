9c303cbf28aa31b7c72becaa5289b616207f247a
kuya-ui <kuyamaxmillan@gmail.com>
2021-09-15 00:42:45 +0300

    changes

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,14 @@
+# from django.db import models
+# # from vote.models import VoteModel
 from django.db import models
+from django.contrib.auth.models import User
+# from tinymce.models import HTMLField
+# from vote.models import VoteModel
 
-# Create your models here.
+#Create your models here.
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=100,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,78 @@
 from django.test import TestCase
+from . models import *
 
 # Create your tests here.
+
+from django.test import TestCase
+from . models import *
+
+class ImageTestClass(TestCase):
+    '''
+    This is a class that perform unnittest  behaviour on the Image Model.
+    '''
+
+    def setUp(self):
+
+        self.image_one = Image(image='',image_name='', image_caption='',likes=40, id=1,user_id=3)
+
+    def test_instance(self):
+        Image.objects.all().delete()
+        self.assertTrue(isinstance(self.image_one,Image)) 
+
+    def test_save_method(self):
+        
+        self.image_one.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_method(self):
+        self.image_one.save_image()
+        self.image_one.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) is 0)
+        
+    def test_update_method(self):
+        self.image_one.save()
+        new_caption = 'That image is cool'
+        done = self.image_one.update_caption(self.image_one.id,new_caption)
+        self.assertEqual(done,new_caption)
+        
+    def tearDown(self):
+        Image.objects.all().delete()   
+    
+       
+        
+        
+class ProfileTestClass(TestCase):
+    
+    '''
+    This is a class that perform unnittest  behaviour on the Profile Model.
+    '''
+
+    def setUp(self):
+        self.profile_one = Profile(profile_photo='',bio='',user_id=3)
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_one,Profile))
+
+    def test_save_method(self):
+
+        self.profile_one.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile_one.save_profile()
+        self.profile_one.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) is 0)
+
+    def test_update_method(self):
+        self.profile_one.save_profile()
+        new_bio = '# thursday speaker'
+        done = self.profile_one.update_bio(self.profile_one.pk,new_bio)
+        self.assertEqual(done,new_bio)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
