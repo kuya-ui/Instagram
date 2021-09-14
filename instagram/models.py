d668a8333fcc36885af3409376d7c4ab2fd655db
kuya-ui <kuyamaxmillan@gmail.com>
2021-09-15 01:19:25 +0300

    add forms

--- instagram/models.py
+++ instagram/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 
 #Create your models here.
 
-class Comments(models.Model):
-    comment = models.CharField(max_length=100,blank=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    image = models.ForeignKey(Image,on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(auto_now_add=True)
+# class Comments(models.Model):
+#     comment = models.CharField(max_length=100,blank=True)
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)
+#     image = models.ForeignKey(Image,on_delete=models.CASCADE)
+#     date_posted = models.DateTimeField(auto_now_add=True)
