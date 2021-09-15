c88a54ddd8496a433e38a3d1c0f280d218edc968
kuya-ui <kuyamaxmillan@gmail.com>
2021-09-15 15:44:47 +0300

    make chsnges

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,5 @@
-# from django.db import models
-# # from vote.models import VoteModel
+from django.db import models
+from vote.models import VoteModel
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
@@ -12,8 +12,31 @@ class Image(VoteModel,models.Model):
     image_caption = models.CharField(max_length=50)
     likes = models.PositiveIntegerField(default=0)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=100,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='images')
     bio = HTMLField()
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-    
+
+    def __str__(self):
+        return self.profile_photo.url
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def update_bio(cls,id,new_bio):
+        cls.objects.filter(pk = id).update(bio=new_bio)
+        new_bio_object = cls.objects.get(bio = new_bio)
+        new_bio = new_bio_object.bio
+        return new_bio
