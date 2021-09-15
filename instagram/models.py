64091229c1b7b8ded74bf8abdfacc9eadbf1f411
kuya-ui <kuyamaxmillan@gmail.com>
2021-09-15 15:54:27 +0300

    add changes

--- instagram/models.py
+++ instagram/models.py
@@ -13,6 +13,27 @@ class Image(VoteModel,models.Model):
     likes = models.PositiveIntegerField(default=0)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
 
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls,id,new_caption):
+        cls.objects.filter(pk = id ).update(image_caption = new_caption)
+        new_caption_object = cls.objects.get(image_caption=new_caption)
+        new_caption = new_caption_object.image_caption
+        return new_caption
+
+    @classmethod
+    def get_single_photo(cls,id):
+        image = cls.objects.get(pk=id)
+        return image
+
+    def __str__(self):
+        return self.image_name
+
 
 class Comments(models.Model):
     comment = models.CharField(max_length=100,blank=True)
