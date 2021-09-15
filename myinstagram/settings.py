dac02db51aaef3d61dc25ad4058e4f0745a47957
kuya-ui <kuyamaxmillan@gmail.com>
2021-09-15 20:51:30 +0300

    changes

--- myinstagram/settings.py
+++ myinstagram/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # DEFAULT_AUTO_FIELD='django.db.models.BigAutoFile'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
